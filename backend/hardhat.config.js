7ae187ab646ea3a545acaca664ac8c1c3e0eda3d
nanaknihal <nanaknihal@gmail.com>
2022-04-30 18:26:25 -0700

    added some more signers, now WTFBios tests pass

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -36,7 +36,12 @@ module.exports = {
   networks: {
     hardhat: {
       from : "0xC8834C1FcF0Df6623Fc8C8eD25064A4148D99388", // Send transactions from this address by default
-      accounts : [{privateKey: process.env.PRIVATE_KEY, balance: "2110000000000000000000000"}], // Private key for the above address
+      accounts : [
+        {privateKey: process.env.PRIVATE_KEY, balance: "2110000000000000000000000"},
+        {privateKey: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', balance: "10000000000000000000000"},
+        {privateKey: '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d', balance: "15100000000000000000000"},
+        
+      ], // Private key for the above address
       forking: {
         url: "https://xdai-archive.blockscout.com/", 
         blockNumber: 21908151
