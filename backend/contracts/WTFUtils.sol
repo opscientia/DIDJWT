f1f6dc8510484b3e0c4ff319091f594782815ede
nanaknihal <nanaknihal@gmail.com>
2022-05-05 19:39:03 -0400

    added comments to WTFUtils

--- backend/contracts/WTFUtils.sol
+++ backend/contracts/WTFUtils.sol
@@ -13,18 +13,21 @@ library WTFUtils {
         } 
     }
 
+    // Covnerts bytes32 to address
     function bytes32ToAddress(bytes32 b_) private pure returns (address addr) {
         assembly {
             addr := mload(add(b_,20)) //shouldn't it be 0x20 or is that equivalent
         } 
     }
 
+    // Converts bytes32 to uint256
     function bytes32ToUInt256(bytes32 b_) public pure returns (uint256 u_) {
         assembly {
             u_ := mload(add(b_,20)) //shouldn't it be 0x20 or is that equivalent
         } 
     }
 
+    // Converts the first 32 bytes of input_ to bytes32
     function bytesToFirst32BytesAsBytes32Type(bytes memory input_) public pure returns (bytes32 b_) {
         assembly {
             // there is probably an easier way to do this
@@ -33,7 +36,8 @@ library WTFUtils {
         } 
     }
 
-    // We need to take the last 32 bytes to obtain the sha256 hash from the the PKCS1-v1_5 padding
+    // Converts the last 32 bytes of input_ to bytes32
+    // (Used to take the last 32 bytes of the PKCS1-v1_5 padded value, which is the sha256 hash of the input)
     function bytesToLast32BytesAsBytes32Type(bytes memory input_) public pure returns (bytes32 b_) {
         assembly {
             // there is probably an easier way to do this
@@ -43,10 +47,12 @@ library WTFUtils {
         }
     }
     
+    // Converts an address to bytes 
     function addressToBytes(address a) public pure returns (bytes memory) {
         return abi.encodePacked(a);
     }
     
+    // Converts bytes32 to bytes
     function bytes32ToBytes(bytes32 b_) public pure returns (bytes memory){
         return abi.encodePacked(b_);
     }
@@ -54,10 +60,12 @@ library WTFUtils {
     //   return abi.encodePacked(a);
     // }
 
+    // Converts a string to bytes
     function stringToBytes(string memory s) public pure returns (bytes memory) {
         return abi.encodePacked(s);
     }
 
+    // Compares equality of bytes
     function bytesAreEqual(bytes memory  a_, bytes memory b_) public pure returns (bool) {
         return (a_.length == b_.length) && (keccak256(a_) == keccak256(b_));
     }
@@ -79,7 +87,8 @@ library WTFUtils {
     //
     // }
 
-    // This could be more efficient by not copying the whole thing -- just the parts that matter
+    // Returns input_[start_ : end_] (slicing operation)
+    // This could be more efficient by not copying the whole thing, rather just the parts that matter
     function sliceBytesMemory(bytes memory input_, uint256 start_, uint256 end_) public view returns (bytes memory r) {
         uint256 len_ = input_.length;
         r = new bytes(len_);
@@ -102,6 +111,7 @@ library WTFUtils {
         }
     }
 
+    // Performs modular exponentiation; returns base ** exponent (mod modulus)
     // BIG thanks to dankrad for this function: https://github.com/dankrad/rsa-bounty/blob/master/contract/rsa_bounty.sol
     // Expmod for bignum operands (encoded as bytes, only base and modulus)
     function modExp(bytes memory base, uint exponent, bytes memory modulus) public view returns (bytes memory o) {
@@ -161,9 +171,8 @@ library WTFUtils {
         return unpadded;
     }
 
-
+    // Decodes base64-encoded bytes
     // Base64 Library modified from https://github.com/Brechtpd/base64/blob/main/base64.sol
- 
     function decodeFromBytes(bytes memory input) public pure returns (bytes memory output) {
         return Base64.decodeFromBytes(input);
     }
